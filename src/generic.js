e1a62e2905bbf8c3e2cb52dd040f8fdf6149ec74
Shampa Khatun <shampacseruet@gmail.com>
2025-09-23 19:18:17 +0600

    practise generics with class, extend interface and type alias

--- src/generic.js
+++ src/generic.js
@@ -29,6 +29,7 @@ var response1 = {
     data: "This is a string"
 };
 console.log("API Response:", response1);
+// generic with class
 var NamedValue = /** @class */ (function () {
     function NamedValue(name) {
         this.name = name;
@@ -47,3 +48,57 @@ var NamedValue = /** @class */ (function () {
 var value = new NamedValue('myNumber');
 value.setValue(10);
 console.log(value.toString());
+var stringContainer = { value: "Hello, Generics!" };
+console.log(stringContainer.value); // Output: Hello, Generics!
+var numbercounter = { value: 42 };
+console.log(numbercounter.value); // Output: 42
+//Generics + extends (Constraint)
+function createLoggedPair(v1, v2) {
+    console.log("creating pair: v1='".concat(v1, "', v2='").concat(v2, "'"));
+    return [v1, v2];
+}
+createLoggedPair("shampa", 25);
+// Output: creating pair: v1='shampa', v2='25'
+// Return: ["shampa", 25]
+createLoggedPair(10, 20);
+// Output: creating pair: v1='10', v2='20'
+// Return: [10, 20]
+createLoggedPair("RUET", "CSE");
+// Output: creating pair: v1='RUET', v2='CSE'
+// Return: ["RUET", "CSE"]
+//Error (boolean not allowed, because only string|number allowed)
+// createLoggedPair(true, "test");
+function createDefaultPair(v1, v2) {
+    return [v1, v2];
+}
+console.log(createDefaultPair("shampa", 25));
+function basic(v1, v2) {
+    return [v1, v2];
+}
+console.log(basic("shampa", true));
+console.log(basic("shampa", 25));
+// console.log(basic(25, "shampa")); // error
+function printObject(obj) {
+    console.log("Object:", obj);
+}
+printObject({ name: "Shampa", age: 25 }); //  OK
+printObject([1, 2, 3]); // OK (array is object)
+//Error: number is not an object
+// printObject(10);
+function getFirst(arr) {
+    return arr[0];
+}
+console.log(getFirst([10, 20, 30]));
+console.log(getFirst(["a", "b", "c"]));
+// Error: boolean not allowed
+// console.log(getFirst([true, false]));
+console.log(getFirst(["RUET", "CSE"]));
+var pair1 = { key: "age", value: 25 };
+console.log(pair1); // { key: 'age', value: 25 }
+var pair2 = { key: 1, value: "one" };
+console.log(pair2); // { key: 1, value: 'one' }
+function printPerson(person) {
+    console.log("".concat(person.name, " is ").concat(person.age, " years old"));
+}
+printPerson({ name: "Shampa", age: 25 });
+printPerson({ name: "Shahariar", age: 27, city: "Dhaka" });
--- src/generic.ts
+++ src/generic.ts
@@ -32,6 +32,8 @@ const response1: APIResponse<string> = {
 
 console.log("API Response:", response1);
 
+
+// generic with class
 class NamedValue<T> {
   private _value: T | undefined;
 
@@ -55,3 +57,92 @@ const value = new NamedValue<number>('myNumber');
 value.setValue(10);
 
 console.log(value.toString());
+
+// generics in type alias
+type Container<T> = { value: T };
+const stringContainer: Container<string> = { value: "Hello, Generics!" };
+console.log(stringContainer.value); // Output: Hello, Generics!
+const numbercounter: Container<number> = { value: 42 };
+console.log(numbercounter.value); // Output: 42
+
+//Generics + extends (Constraint)
+function createLoggedPair<S extends string | number, T extends string | number>(v1: S, v2: T): [S, T] {
+  console.log(`creating pair: v1='${v1}', v2='${v2}'`);
+  return [v1, v2];
+}
+createLoggedPair("shampa", 25);
+// Output: creating pair: v1='shampa', v2='25'
+// Return: ["shampa", 25]
+
+createLoggedPair(10, 20);
+// Output: creating pair: v1='10', v2='20'
+// Return: [10, 20]
+
+createLoggedPair("RUET", "CSE");
+// Output: creating pair: v1='RUET', v2='CSE'
+// Return: ["RUET", "CSE"]
+
+//Error (boolean not allowed, because only string|number allowed)
+// createLoggedPair(true, "test");
+
+function createDefaultPair<S extends string | number = string, T extends string | number = number>(
+  v1: S,
+  v2: T
+): [S, T] {
+  return [v1, v2];
+}
+
+console.log(createDefaultPair("shampa", 25));
+
+function basic<S extends string, T extends number | boolean>
+(v1:S, v2:T):[S,T]{
+    return [v1,v2];
+}
+console.log(basic("shampa", true));
+console.log(basic("shampa", 25));
+// console.log(basic(25, "shampa")); // error
+
+
+function printObject<T extends object>(obj: T) {
+  console.log("Object:", obj);
+}
+
+printObject({ name: "Shampa", age: 25 }); //  OK
+printObject([1, 2, 3]);                   // OK (array is object)
+
+//Error: number is not an object
+// printObject(10);
+
+function getFirst<T extends string | number>(arr: T[]): T {
+  return arr[0];
+}
+
+console.log(getFirst([10, 20, 30]));       
+console.log(getFirst(["a", "b", "c"]));    
+
+// Error: boolean not allowed
+// console.log(getFirst([true, false]));
+console.log(getFirst(["RUET", "CSE"]));    
+
+//generic interface
+interface Pair<K, V> {
+  key: K;
+  value: V;
+}   
+const pair1: Pair<string, number> = { key: "age", value: 25 };
+console.log(pair1); // { key: 'age', value: 25 }
+const pair2: Pair<number, string> = { key: 1, value: "one" };
+console.log(pair2); // { key: 1, value: 'one' }
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function printPerson<T extends Person>(person: T) {
+  console.log(`${person.name} is ${person.age} years old`);
+}
+
+printPerson({ name: "Shampa", age: 25 }); 
+printPerson({ name: "Shahariar", age: 27, city: "Dhaka" })
+
