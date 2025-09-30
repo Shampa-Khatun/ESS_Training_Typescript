5a8f679a07e17f1c7903ba45ea331b08f515a08e
Shampa Khatun <shampacseruet@gmail.com>
2025-09-30 13:10:06 +0600

    practise awaited utility types

---
+++ src/utility-types/awaited.ts
@@ -0,0 +1,33 @@
+/*JavaScript e jodi amra await use kori, seta Promise resolve kore inner value return kore.
+TypeScript er problem holo – amra type system k easily bujhate parina je await er pore exactly ki type ashbe.
+Ei gap fill korar jonne Awaited<T> asche.*/
+
+// await in async functions or .then() methods on Promises - unwrap Promise types
+// promise resolved type
+type A = Awaited<Promise<string>>;
+type B = Awaited<Promise<Promise<number>>>;
+type C = Awaited<string | Promise<number>>;
+type D = Awaited<Promise<string | Promise<number>>>;
+type E = Awaited<boolean>;
+type F = Awaited<null>;
+type G = Awaited<undefined>;
+type H = Awaited<never>;
+
+interface thenable {
+  then(onfulfilled: (value: number) => void): void;
+}
+type I = Awaited<thenable>; // number
+
+// before TS 4.5, people used custom utility types like UnwrapPromise<T> or wrote complex conditional types.
+
+async function example(): Promise<string> {
+  return 'Hello, World!';
+}
+type J = Awaited<typeof example>; // ()=>Promise<string>
+type K = Awaited<ReturnType<typeof example>>; // string
+
+async function getUser() {
+  return { id: 1, name: 'Bob' };
+}
+type User = Awaited<ReturnType<typeof getUser>>;
+// { id: number; name: string }
